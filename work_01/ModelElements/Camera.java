101bdd4ec60d0334f380603d81e80bef4b4f68d7
Denis <elspy@mail.ru>
2023-10-10 20:50:22 +0300

    Create files and folders for a work_01.

---
+++ work_01/ModelElements/Camera.java
@@ -0,0 +1,10 @@
+package ModelElements;
+
+public class Camera {
+    public Point3D location;
+    public Angle3D angle;
+
+    public void Rotate(Angle3D);
+
+    public void Move(Point3D); 
+}
---
+++ work_01/ModelElements/Scene.java
@@ -0,0 +1,17 @@
+package ModelElements;
+
+import java.util.List;
+
+public class Scene {
+    public int id;
+    public List<PoligonalModel> models;
+    public List<Flash> flashs;
+
+    public method1(Type) {
+        return Type;
+    }
+
+    public method2(Type, Type) {
+        return Type;
+    }
+}
